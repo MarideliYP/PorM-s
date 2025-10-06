0f28b416ef3fa5870f16086f26b8b8b0c79dc8ff
MarideliYP <137637804+MarideliYP@users.noreply.github.com>
2025-10-06 07:57:52 -0400

    Update settings.py

--- PrMas/settings.py
+++ PrMas/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-
+import dj_database_url
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -68,10 +68,7 @@ AUTH_USER_MODEL = 'user.User'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
